951ed38d9f7cc340b4bfef47d83927df6de024a0
Danial Siddiqi <66087554+danialaely@users.noreply.github.com>
2025-01-30 16:54:30 +0500

    First Commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Animator cameraAnimator;
+    float speed = 30.0f;
+    bool moveRight;
+
+    //public Cube cb;
+
+    private void Start()
+    {
+        moveRight = true;
+    }
+
+    private void Update()
+    {
+        StartCoroutine(MovingForward(3.0f));
+    }
+
+    IEnumerator MovingForward(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        cameraAnimator.enabled = false;
+        speed += 0.01f;
+       // if (cb.hit == false) {}
+        if (moveRight)
+        {
+            transform.position += new Vector3(0, 0, 1.0f) * speed * Time.deltaTime;
+        }
+        else 
+        {
+            transform.position += new Vector3(-1.0f, 0, 0) * speed * Time.deltaTime;
+        }
+    }
+
+    public void SetMoveRight(bool right) 
+    {
+        moveRight=right;
+    }
+
+}
